b4f98f87745ad8d301b4b5ac38f92a0ee4642654
Allan J. Martínez <Jafectnez@gmail.com>
2019-04-15 23:23:48 -0600

    Pequeña mejora visual de la pantalla Ingresar_resultado

--- Ingresar_resultados.php
+++ Ingresar_resultados.php
@@ -10,21 +10,22 @@
 	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
 	<link rel="stylesheet" type="text/css" href="css/barra-menu.css">
 	<link rel="stylesheet" type="text/css" href="css/carousel.css">
+	<link rel="stylesheet" type="text/css" href="css/styleBarra.css">
 	<link rel="stylesheet" type="text/css" href="css/materialize.min.css">
 	
 </head>
 <body>
 
 	<!--Contenedor-->
-	<div class="container-fluid">
+	<div class="container-fluid" style="padding: 0;">
 		<div class="row">
 
 		<!--Aqui Esta Contenida La Barra De Menu-->
-		<div id="barraNav" class="col-lg-3 lista"></div>
+		<div id="barraNav" class="col-lg-2 lista"></div>
         <!--Aqui Esta Finaliza La La Barra De Menu-->
 
-			<div id="div-examenes" class="col-lg-9 col-md-9 well" style="border: black 1px solid; background-color: #DBEBC6;">
-				
+			<div id="div-examenes" class="col-xl-10 col-lg-10 col-md-6 col-sm-6 well" style="border: black 1px solid; background-color: #DBEBC6;">
+				<h4>Ingresar Resultados</h4>
 				<!--Contenido-->
 				 <ul class="collapsible" id="ul-area">
 				 	
