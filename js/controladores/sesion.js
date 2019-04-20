111c7da116ea1131784c49ec98489be0273ac1ad
Allan J. Martínez <Jafectnez@gmail.com>
2019-04-20 13:28:24 -0600

    pequeños cambios en identacion, sigue sin funcionarme el login

--- js/controladores/sesion.js
+++ js/controladores/sesion.js
@@ -1,34 +1,32 @@
 function iniciarSesion(){
-      var usuario= $("#txt-Usuario").val();
-      var contrasenia= $("#txt-Password").val();
+  var usuario= $("#txt-Usuario").val();
+  var contrasenia= $("#txt-Password").val();
 
-       $.ajax({
-
-   url:"ajax/acciones-sesion.php",
+  $.ajax({
+    url:"ajax/acciones-sesion.php",
     data:{
-            "accion":"iniciar-sesion",
-            "txt-Usuario":usuario,
-            "txt-Password":contrasenia
-          },
-    dataType: 'json ',
+          "accion":"iniciar-sesion",
+          "txt-Usuario":usuario,
+          "txt-Password":contrasenia
+        },
+    dataType: 'json',
     method: "POST",
     success: function(respuesta){  
-    console.log(respuesta) ;
-           if (respuesta.loggedin==0) {
-
-              $("#status").html(respuesta.mensaje);
-            }
-            else {
-               if (respuesta.tipo_usuario==1) {
-                  window.location="catalogo.php";
-               }
-               else if (respuesta.tipo_usuario==2) {
-                  window.location="catalogo.php";
-               }  
-            }
+      console.log(respuesta) ;
+      if (respuesta.loggedin==0) {
+        $("#status").html(respuesta.mensaje);
+      }
+      else {
+        if (respuesta.tipo_usuario==1) {
+          window.location="catalogo.php";
+        }
+        else if (respuesta.tipo_usuario==2) {
+          window.location="catalogo.php";
+        }  
+      }
     },
     error:function(e){
-        console.log(e);
+      console.log(e);
     }
   });
 }
