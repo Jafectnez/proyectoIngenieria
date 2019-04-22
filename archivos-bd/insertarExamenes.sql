23d042730ee5d9656d719209645616534fcb5727
solimanhub <david.soliman2010@gmail.com>
2019-04-22 04:01:15 -0600

    insertarExamenes.sql es una consulta con una pequeña lista de examenes, para hacer pruebas

---
+++ archivos-bd/insertarExamenes.sql
@@ -0,0 +1,8 @@
+INSERT INTO `TBL_EXAMENES` (`ID_EXAMEN`, `ID_AREA`, `NOMBRE`, `PRECIO`, `DESCRIPCION`, `TIEMPO_ANALISIS`) VALUES
+(null, 1, 'Hemograma', 400, 'Examen de Sangre', '1 dia'),
+(null, 1, 'Urinalisis completo', 400, 'Descripcion de relleno', 'dia'),
+(null, 1, 'Heces por parasito', 400, 'Descripcion de relleno', 'dia'),
+(null, 1, 'Perfil renal', 400, 'Descripcion de relleno', 'dia'),
+(null, 1, 'Perfil lipidico', 400, 'Descripcion de relleno', 'dia'),
+(null, 1, 'Perfil hepatico', 400, 'Descripcion de relleno', 'dia'),
+(null, 1, 'Perfil triode', 400, 'Descripcion de relleno', 'dia');
